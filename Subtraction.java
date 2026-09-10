8e774e60c442c22adc2156b5f6db0dcd064924f9
ShaheenDisi <shaheendisi69@gmail.com>
2026-09-10 21:14:57 +0300

    Add files via upload

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+package com.ttbb.myfirsttry;
+public class Subtraction implements Operation {
+
+    @Override
+    public double calculate(double a, double b) {
+        return a - b;
+    }
+    
+}
